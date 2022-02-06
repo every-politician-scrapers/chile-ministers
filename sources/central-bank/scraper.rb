c6124d97f665735a2b236caa80cf2f683a608e54
Tony Bowden <tony@tmtm.com>
2022-02-06 10:58:22 +0200

    update scraper for eps 0.3

--- sources/central-bank/scraper.rb
+++ sources/central-bank/scraper.rb
@@ -4,33 +4,6 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-# Spanish dates
-class SpanishExtd < WikipediaDate
-  REMAP = {
-    'A la fecha' => '',
-    'enero'      => 'January',
-    'febrero'    => 'February',
-    'marzo'      => 'March',
-    'abril'      => 'April',
-    'mayo'       => 'May',
-    'junio'      => 'June',
-    'julio'      => 'July',
-    'agosto'     => 'August',
-    'septiembre' => 'September',
-    'octubre'    => 'October',
-    'noviembre'  => 'November',
-    'diciembre'  => 'December',
-  }.freeze
-
-  def date_en
-    super.gsub(' de ', ' ').tidy
-  end
-
-  def remap
-    super.merge(REMAP)
-  end
-end
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
   decorator UnspanAllTables
@@ -43,14 +16,6 @@ class OfficeholderList < OfficeholderListBase
   class Officeholder < OfficeholderBase
     def columns
       %w[img name dates].freeze
-    end
-
-    def date_class
-      SpanishExtd
-    end
-
-    def tds
-      noko.css('td,th')
     end
   end
 end
