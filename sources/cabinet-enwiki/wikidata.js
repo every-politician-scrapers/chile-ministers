9d3dcdfd9957cd30b97b7a65c997a5aa70bc373a
Tony Bowden <tony@tmtm.com>
2022-02-06 11:10:42 +0200

    Ignore anyone starting in the future

--- sources/cabinet-enwiki/wikidata.js
+++ sources/cabinet-enwiki/wikidata.js
@@ -17,9 +17,8 @@ module.exports = function () {
           ?held ps:P39 ?positionItem ; pq:P580 ?startDate .
           FILTER NOT EXISTS { ?held wikibase:rank wikibase:DeprecatedRank }
           OPTIONAL { ?held pq:P582 ?endDate }
-          FILTER (!BOUND(?endDate) || (
-            (?endDate < NOW()) && (?endDate > "${meta.cabinet.start}"^^xsd:dateTime)
-          ))
+          FILTER (!BOUND(?endDate) || (?endDate > "${meta.cabinet.start}"^^xsd:dateTime))
+          FILTER (?startDate <= NOW())
 
           OPTIONAL {
             ?held prov:wasDerivedFrom ?ref .
